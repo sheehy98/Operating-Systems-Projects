1a36176ff86e8ce1a0263e2415bab9e9c8b6d901
Hamayel Qureshi <hqureshi@wpi.edu>
2021-02-11 18:54:29 -0500

    refactoring code. old version still up. gang gang.

--- Project2/scheduler.c
+++ Project2/scheduler.c
@@ -4,284 +4,239 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h> // importing string.h... are we allowed to use this?
+#include "scheduler_help.h"
 
-#define MAX_LINES 512
+// #define MAX_LINES 512
+// #define FIFO 1
+// #define SJF 2
+// #define RR 3
 
-struct job // job linkedlist node
-{
-    int id;
-    int length;
-    // other meta data to be added?
-    struct job *next;
-};
+// struct job // job linkedlist node
+// {
+//     int id;
+//     int length;
+//     // other meta data to be added?
+//     struct job *next; //
+// };
 
 int main(int argc, char **argv)
 {
+
     struct job *head = NULL;
-    struct job *current = NULL;
-    char fileLines[5][MAX_LINES]; // setting a max of 512 lines from input file, don't think it'll exceed
     char *policy = argv[1];
     char *testFile = argv[2];
     char *timeSlice = argv[3];
-    int timeSliceInt;
-    if (timeSlice != NULL)
+    int timeSliceInt = timeSlice != NULL ? atoi(timeSlice) : 0;
+
+    if (checkValidInput(argc, policy, timeSliceInt) == 1)
     {
-        timeSliceInt = atoi(timeSlice); // THIS IS CAUSING A SEG FAULT AHHHH
-    }
-
-    void removeJob(struct job * *head, int key)
-    {
-        // store curr head node
-        struct job *temp = *head, *prev;
-
-        // if head has key (id) to delete
-        if (temp != NULL && temp->id == key)
+        if (openFile(&head, testFile) != 0)
         {
-            *head = temp->next; // head changed
-            free(temp);         //free the olde ahh head
-            return;
-        }
-
-        // else search for key to be added
-        while (temp != NULL && temp->id != key)
-        {
-            prev = temp;
-            temp = temp->next;
-        }
-
-        // if key not in linked list
-        if (temp == NULL)
-        {
-            printf("Could not find ID to delete :(");
-            return;
-        }
-
-        // else unlink
-        prev->next = temp->next;
-
-        free(temp);
-    }
-
-    /* Insert job at end of linked list as opposed to at start (first in) */
-    void appendJob(struct job * *head, int id, int length)
-    {
-        /* make new node */
-        struct job *link = (struct job *)malloc(sizeof(struct job));
-
-        struct job *last = *head;
-
-        /* add data */
-        link->id = id;
-        link->length = length;
-
-        /* will be last node */
-        link->next = NULL;
-
-        /* if list empt, new node is head */
-        if (*head == NULL)
-        {
-            *head = link;
-            return;
-        }
-
-        /* otherwise go till last node */
-        while (last->next != NULL)
-            last = last->next;
-
-        /* change last node next */
-        last->next = link;
-        return;
-    }
-
-    void openFile()
-    {
-        int idCounter = 0; // use this value to iterate backwards later on
-        FILE *fp = fopen(testFile, "r");
-        int curLine = 0;
-
-        // Storing data FIFO with append job function
-        if (fp != NULL)
-        {
-
-            while ((fgets(fileLines[idCounter], MAX_LINES, fp) != NULL))
+            switch (getPolicy(policy))
             {
-                appendJob(&head, idCounter, atoi(fileLines[idCounter]));
-                idCounter++;
+            case (FIFO):
+            {
+                // printJobs(head);
+                printf("Execution trace with %s:\n", policy);
+                struct job *ptr = head;
+                if (ptr == NULL)
+                    printf("THE HEAD IS NULL WTH!!\n");
+                while (ptr != NULL)
+                {
+                    printf("Job %d ran for: %d\n", ptr->id, ptr->length);
+                    ptr = ptr->next;
+                }
+                printf("End of execution with %s.\n", policy);
+                break;
+                // case (0):
+            }
+            case (SJF):
+            {
+                printf("Execution trace with %s:\n", policy);
+                bubbleSortSJF(&head);
+                struct job *ptr = head;
+                while (ptr != NULL)
+                {
+                    printf("Job %d ran for: %d\n", ptr->id, ptr->length);
+                    ptr = ptr->next;
+                }
+                printf("End of execution with %s.\n", policy);
+                break;
+            }
+            case (RR):
+            {
+                printf("Execution trace with %s:\n", policy);
+                struct job *ptr = head;
+                while (1)
+                {
+                    int newLength = 0;
+                    if (ptr->length > 0)
+                    {
+                        // if job length is greater than time slice
+                        if (ptr->length > timeSliceInt)
+                        {
+                            printf("Job %i ran for: %i\n", ptr->id, timeSliceInt);
+                            newLength = ptr->length - timeSliceInt;
+
+                            appendJob(&ptr, ptr->id, newLength);
+                        }
+                        else
+                        {
+                            printf("Job %i ran for: %i\n", ptr->id, ptr->length);
+                        }
+                        if ((ptr = ptr->next) == NULL)
+                            break;
+                    }
+                }
+                printf("End of execution with %s.\n", policy);
+                break;
+            }
+            default:
+                printf("Didn't work\n");
+                break;
             }
         }
         else
         {
-            printf("Could not find specified file. Check your argument and try again.\n");
-            exit(1);
-        }
-    }
-
-    void swapSJF(struct job * a, struct job * b)
-    {
-        int tempId = a->id;
-        int tempLength = a->length;
-        a->length = b->length;
-        a->id = b->id;
-        b->length = tempLength;
-        b->id = tempId;
-    }
-
-    void bubbleSortSJF(struct job * head)
-    {
-        int swapped, i;
-        struct job *ptrL;
-        struct job *ptrR;
-
-        if (head == NULL)
-            return;
-
-        do
-        {
-            swapped = 0;
-            ptrL = head;
-
-            while (ptrL->next != ptrR)
-            {
-                if (ptrL->length > ptrL->next->length)
-                {
-                    swapSJF(ptrL, ptrL->next);
-                    swapped = 1;
-                }
-                ptrL = ptrL->next;
-            }
-            ptrR = ptrL;
-        } while (swapped);
-    }
-
-    // if not providing between 3 and 4 arguments, scream!
-    if (!(argc >= 3 && argc <= 4))
-    {
-        printf("Requires at least 2 arguments: Policy, tests/x.in.\n");
-        return 0;
-    }
-
-    // time slice is invalid, should be between 0 and 100 realistically
-    if (!(timeSliceInt >= 0 && timeSliceInt < 100))
-    {
-        printf("Time slice value invalid. Choose between 0 and 100.\n");
-        return 0;
-    }
-
-    /* Open le file, fill le array */
-    openFile();
-
-    // switch case with if else, not working with ints could use enum to create a struct
-    if (strcmp(policy, "FIFO") == 0)
-    {
-        printf("Execution trace with %s:\n", policy);
-        int idCounter = 0; // use this value to iterate backwards later on
-        struct job *ptr = head;
-        while (ptr != NULL)
-        {
-            printf("Job %d ran for: %d\n", ptr->id, ptr->length);
-            ptr = ptr->next;
-            idCounter--;
-        }
-        printf("End of execution with %s.\n", policy);
-    }
-
-    else if (strcmp(policy, "SJF") == 0)
-    {
-        printf("Execution trace with %s:\n", policy);
-        bubbleSortSJF(head);
-        struct job *ptr = head;
-        while (ptr != NULL)
-        {
-            printf("Job %d ran for: %d\n", ptr->id, ptr->length);
-            ptr = ptr->next;
-        }
-        printf("End of execution with %s.\n", policy);
-    }
-
-    else if (strcmp(policy, "RR") == 0)
-    {
-        // KEEP TRACK OF:
-        // length of time slice provided
-        // length of job
-        // HOW TO DO:
-        // if (time > 0) {
-        // if time > than time slice
-        // print out job ran for timeslice
-        // subtract joblength - timeslice
-        // add back to queue
-        // else if time <= time slice
-        // }
-        // run job for allotted amount of time
-        // somehow remove
-
-        if (argc != 4)
-        {
-            printf("You must provide a time slice value for RR\n");
+            printf("Could not find specified file. Check your arguments and try again.\n");
             return 0;
         }
-        // struct job *ptr = head;
-        printf("Execution trace with %s:\n", policy);
-        struct job *ptr = head;
-        while (1)
-        {
-            // int isFinished = 1;
-            int newLength = 0;
-
-            if (ptr->length > 0)
-            {
-                // isFinished = 0; // pending process
-
-                // if job length is greater than time slice
-                if (ptr->length > timeSliceInt)
-                {
-                    printf("Job %i ran for: %i\n", ptr->id, timeSliceInt);
-                    newLength = ptr->length - timeSliceInt;
-                    // removeJob(&ptr, ptr->id);
-                    appendJob(&ptr, ptr->id, newLength);
-                }
-                else
-                {
-                    printf("Job %i ran for: %i\n", ptr->id, ptr->length);
-                    // removeJob(&ptr, ptr->id);
-                }
-                if ((ptr = ptr->next) == NULL)
-                    break;
-            }
-
-            // if (isFinished == 1)
-            // {
-            //     printf("BREAK\n");
-            //     break;
-            // }
-        }
-        printf("End of execution with %s.\n", policy);
-
-        // while (ptr != NULL)
-        // {
-        //     int timeLeft;
-
-        //     if (ptr->length > 0)
-        //     {
-        //         if (ptr->length > timeSliceInt)
-        //         {
-        //             printf("Job %i ran for: %i\n", head->id, timeSliceInt);
-        //             timeLeft = ptr->length - timeSliceInt;
-        //             appendJob(&ptr, ptr->id, timeLeft);
-        //             ptr = ptr->next;
-        //         }
-        //         // else
-        //         {
-        //             printf("Job %i ran for: %i\n", head->id, ptr->length);
-        //             removeJob(&ptr, ptr->id);
-        //             ptr = ptr->next;
-        //         }
-        //     }
-        // }
     }
     else
     {
-        printf("Enter FIFO SJF or RR as the policy\n");
+        printf("Check input and try again.\n");
+        return 0;
     }
+
+    // // if not providing between 3 and 4 arguments, scream!
+    // if (!(argc >= 3 && argc <= 4))
+    // {
+    //     printf("Requires at least 2 arguments: Policy, tests/x.in.\n");
+    //     return 0;
+    // }
+
+    // // time slice is invalid, should be between 0 and 100 realistically
+    // if (!(timeSliceInt >= 0 && timeSliceInt < 100))
+    // {
+    //     printf("Time slice value invalid. Choose between 0 and 100.\n");
+    //     return 0;
+    // }
+
+    // /* Open le file, fill le array */
+    // openFile();
+
+    // // switch case with if else, not working with ints could use enum to create a struct
+    // if (strcmp(policy, "FIFO") == 0)
+    // {
+    //     printf("Execution trace with %s:\n", policy);
+    //     int idCounter = 0; // use this value to iterate backwards later on
+    //     struct job *ptr = head;
+    //     while (ptr != NULL)
+    //     {
+    //         printf("Job %d ran for: %d\n", ptr->id, ptr->length);
+    //         ptr = ptr->next;
+    //         idCounter--;
+    //     }
+    //     printf("End of execution with %s.\n", policy);
+    // }
+
+    // else if (strcmp(policy, "SJF") == 0)
+    // {
+    //     printf("Execution trace with %s:\n", policy);
+    //     bubbleSortSJF(head);
+    //     struct job *ptr = head;
+    //     while (ptr != NULL)
+    //     {
+    //         printf("Job %d ran for: %d\n", ptr->id, ptr->length);
+    //         ptr = ptr->next;
+    //     }
+    //     printf("End of execution with %s.\n", policy);
+    // }
+
+    // else if (strcmp(policy, "RR") == 0)
+    // {
+    //     // KEEP TRACK OF:
+    //     // length of time slice provided
+    //     // length of job
+    //     // HOW TO DO:
+    //     // if (time > 0) {
+    //     // if time > than time slice
+    //     // print out job ran for timeslice
+    //     // subtract joblength - timeslice
+    //     // add back to queue
+    //     // else if time <= time slice
+    //     // }
+    //     // run job for allotted amount of time
+    //     // somehow remove
+
+    //     if (argc != 4)
+    //     {
+    //         printf("You must provide a time slice value for RR\n");
+    //         return 0;
+    //     }
+    //     // struct job *ptr = head;
+    //     printf("Execution trace with %s:\n", policy);
+    //     struct job *ptr = head;
+    //     while (1)'
+    //     {
+    //         // int isFinished = 1;
+    //         int newLength = 0;
+
+    //         if (ptr->length > 0)
+    //         {
+    //             // isFinished = 0; // pending process
+
+    //             // if job length is greater than time slice
+    //             if (ptr->length > timeSliceInt)
+    //             {
+    //                 printf("Job %i ran for: %i\n", ptr->id, timeSliceInt);
+    //                 newLength = ptr->length - timeSliceInt;
+    //                 // removeJob(&ptr, ptr->id);
+    //                 appendJob(&ptr, ptr->id, newLength);
+    //             }
+    //             else
+    //             {
+    //                 printf("Job %i ran for: %i\n", ptr->id, ptr->length);
+    //                 // removeJob(&ptr, ptr->id);
+    //             }
+    //             if ((ptr = ptr->next) == NULL)
+    //                 break;
+    //         }
+
+    //         // if (isFinished == 1)
+    //         // {
+    //         //     printf("BREAK\n");
+    //         //     break;
+    //         // }
+    //     }
+    //     printf("End of execution with %s.\n", policy);
+
+    //     // while (ptr != NULL)
+    //     // {
+    //     //     int timeLeft;
+
+    //     //     if (ptr->length > 0)
+    //     //     {
+    //     //         if (ptr->length > timeSliceInt)
+    //     //         {
+    //     //             printf("Job %i ran for: %i\n", head->id, timeSliceInt);
+    //     //             timeLeft = ptr->length - timeSliceInt;
+    //     //             appendJob(&ptr, ptr->id, timeLeft);
+    //     //             ptr = ptr->next;
+    //     //         }
+    //     //         // else
+    //     //         {
+    //     //             printf("Job %i ran for: %i\n", head->id, ptr->length);
+    //     //             removeJob(&ptr, ptr->id);
+    //     //             ptr = ptr->next;
+    //     //         }
+    //     //     }
+    //     // }
+    // }
+    // else
+    // {
+    //     printf("Enter FIFO SJF or RR as the policy\n");
+    // }
 
     return 0;
     // printf("Hello, please help me schedule!");
---
+++ Project2/scheduler_help.h
@@ -0,0 +1,202 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h> // importing string.h... are we allowed to use this?
+// omfg pointers r confusing lomaoafjosdj
+#define MAX_LINES 512
+#define FIFO 1
+#define SJF 2
+#define RR 3
+
+struct job // job linked list struct
+{
+    int id;
+    int length;
+    struct job *next; // singly linked list :(
+};
+
+// function prototypes
+int openFile(struct job **head, char testFile[]);
+int checkValidInput(int count, char policy[], int timeSlice);
+int getPolicy(char policy[]);
+void appendJob(struct job **head, int id, int length);
+void removeJob(struct job **head, int key);
+void swapSJF(struct job *a, struct job *b);
+void bubbleSortSJF(struct job **head);
+void printJobs(struct job *head);
+
+int checkValidInput(int count, char policy[], int timeSlice)
+{
+    int argCountValid = (count <= 4 && count >= 3) ? 1 : 0;
+    int policyValid = (strcmp(policy, "FIFO") == 0 || strcmp(policy, "SJF") == 0 || strcmp(policy, "RR") == 0) ? 1 : 0;
+    int timeSliceValid = (timeSlice > 0 && timeSlice <= 100) ? 1 : 0;
+
+    if ((argCountValid && policyValid && timeSliceValid) == 1)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int getPolicy(char policy[])
+{
+    if (strcmp(policy, "FIFO") == 0)
+    {
+        return FIFO;
+    }
+    else if (strcmp(policy, "SJF") == 0)
+    {
+        return SJF;
+    }
+    else if (strcmp(policy, "RR") == 0)
+    {
+        return RR;
+    }
+    else
+        return 0;
+}
+
+int openFile(struct job **head, char testFile[])
+{
+    char fileLines[5][MAX_LINES]; // setting a max of 512 lines from input file, don't think it'll exceed
+    int idCounter = 0;            // use this value to iterate backwards later on
+    FILE *fp = fopen(testFile, "r");
+    int curLine = 0;
+
+    // Storing data FIFO with append job function
+    if (fp != NULL)
+    {
+
+        while ((fgets(fileLines[idCounter], MAX_LINES, fp) != NULL))
+        {
+            appendJob(head, idCounter, atoi(fileLines[idCounter]));
+            idCounter++;
+        }
+        fclose(fp);
+        return 1;
+    }
+    return 0;
+}
+
+void printJobs(struct job *head)
+{
+    struct job *ptr = (struct job *)malloc(sizeof(struct job));
+    while (ptr != NULL)
+    {
+        printf("ID: %i, Length: %i\n", ptr->id, ptr->length);
+        ptr = ptr->next;
+    }
+}
+
+void printJob(struct job **head)
+{
+}
+
+void appendJob(struct job **head, int id, int length)
+{
+
+    /* make new node */
+    struct job *link = (struct job *)malloc(sizeof(struct job));
+
+    struct job *last = *head;
+
+    /* add data */
+    link->id = id;
+    link->length = length;
+
+    /* will be last node */
+    link->next = NULL;
+
+    /* if list empt, new node is head */
+    if (*head == NULL)
+    {
+        *head = link;
+        return;
+    }
+
+    /* otherwise go till last node */
+    while (last->next != NULL)
+        last = last->next;
+
+    /* change last node next */
+    last->next = link;
+    return;
+}
+
+// void removeJob(struct job **head, int key)
+// {
+//     // store curr head node
+//     struct job *temp = *head, *prev;
+
+//     // if head has key (id) to delete
+//     if (temp != NULL && temp->id == key)
+//     {
+//         *head = temp->next; // head changed
+//         free(temp);         //free the olde ahh head
+//         return;
+//     }
+
+//     // else search for key to be added
+//     while (temp != NULL && temp->id != key)
+//     {
+//         prev = temp;
+//         temp = temp->next;
+//     }
+
+//     // if key not in linked list
+//     if (temp == NULL)
+//     {
+//         printf("Could not find ID to delete :(");
+//         return;
+//     }
+
+//     // else unlink
+//     prev->next = temp->next;
+
+//     free(temp);
+//     return;
+// }
+
+/* Swap helper function for bubble sort algo */
+void swapSJF(struct job *a, struct job *b)
+{
+    printf("Swapping ID: %i with ID%i", a->id, b->id);
+    int tempId = a->id;
+    int tempLength = a->length;
+    a->length = b->length;
+    a->id = b->id;
+    b->length = tempLength;
+    b->id = tempId;
+    return;
+}
+
+/* Generic bubble sort implementation */
+void bubbleSortSJF(struct job **head)
+{
+    int swapped, i;
+    struct job *ptrL;
+    struct job *ptrR;
+
+    if (head == NULL)
+    {
+        return;
+        printf("FFFFF");
+    }
+
+    do
+    {
+        swapped = 0;
+        ptrL = *head;
+        while (ptrL->next != ptrR)
+        {
+            if (ptrL->length > ptrL->next->length)
+            {
+                swapSJF(ptrL, ptrL->next);
+                swapped = 1;
+            }
+            ptrL = ptrL->next;
+        }
+        ptrR = ptrL;
+    } while (swapped);
+}
