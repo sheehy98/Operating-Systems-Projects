e02bd36c6367e5d5d1fd52dd5a2b61fb024edbfe
sheehy98 <37912697+sheehy98@users.noreply.github.com>
2021-03-01 19:24:32 -0500

    Update goatmalloc.c

--- project3/goatmalloc.c
+++ project3/goatmalloc.c
@@ -22,11 +22,11 @@ int init(size_t size)
   printf("...requested size %.00f bytes\n", (double)size); // casting to double to convert -1 to expected output
 
   if ((double)size > MAX_ARENA_SIZE) // casting to double to convert -1 to expected output
-  {
-    printf("...error: requested size larger than MAX_ARENA_SIZE (%d)\n", MAX_ARENA_SIZE);
-    statusno = ERR_BAD_ARGUMENTS;
-    return statusno;
-  }
+    {
+      printf("...error: requested size larger than MAX_ARENA_SIZE (%d)\n", MAX_ARENA_SIZE);
+      statusno = ERR_BAD_ARGUMENTS;
+      return statusno;
+    }
 
   printf("...pagesize is %i bytes\n", (int)pageSize);
   printf("...adjusting size with page boundaries\n");
@@ -63,19 +63,19 @@ int destroy()
   printf("Destroying Arena:\n");
 
   if (_arena_start == 0)
-  {
-    printf("...error: cannot destroy uninitialized arena. Setting error status\n");
-    statusno = ERR_UNINITIALIZED;
-    return statusno;
-  }
+    {
+      printf("...error: cannot destroy uninitialized arena. Setting error status\n");
+      statusno = ERR_UNINITIALIZED;
+      return statusno;
+    }
 
   printf("...unmapping arena with munmap()\n");
   if (munmap(_arena_start, _arena_size) == -1)
-  {
-    printf("...error: system call failed");
-    statusno = ERR_SYSCALL_FAILED;
-    return statusno;
-  }
+    {
+      printf("...error: system call failed");
+      statusno = ERR_SYSCALL_FAILED;
+      return statusno;
+    }
 
   _arena_start = 0;
   _arena_size = 0;
@@ -87,57 +87,57 @@ void *walloc(size_t size)
   statusno = 0;
 
   if (!_arena_start)
-  {
-    printf("Error: Unitialized. Setting status code\n");
-    statusno = ERR_UNINITIALIZED;
-    return NULL; // returns address of status no?
-  }
+    {
+      printf("Error: Unitialized. Setting status code\n");
+      statusno = ERR_UNINITIALIZED;
+      return NULL; // returns address of status no?
+    }
 
   printf("Allocating memory:\n");
   printf("...looking for free chunk of >= %d bytes\n", (int)size);
 
   node_t *mem = _arena_start;
   while (mem != NULL)
-  {
-    if (mem->is_free == 1 && (mem->size >= size)) // first node free and big enough
     {
-      printf("...found free chunk of %d bytes with header at %p\n", (int)mem->size, mem);
-      mem->is_free = 0;
-      mem->size = size;
-      printf("...free chunk->fwd currently points to %p\n", mem->fwd);
-      printf("...free chunk->bwd currently points to %p\n", mem->bwd);
-      printf("...checking if splitting is required\n");
-      /* 
-       * Below line: getting new size to pass into the fwd node stores
-       * how many bytes of memory can be stored after current mem
-       */
-      size_t newSize = (_arena_size - ((size_t)mem - (size_t)_arena_start)) - (size + (2 * sizeof(node_t)));
-      if (mem->fwd == NULL && ((int)newSize >= 1)) // last node
-      {
-        node_t *fwd = (node_t *)((char *)mem + size + sizeof(node_t));
-        fwd->size = newSize;
-        fwd->is_free = 1;
-        fwd->fwd = NULL;
-        fwd->bwd = mem;
-        mem->fwd = fwd;
-      }
-      /* 
-       * else not enough space & last node, set curr size 
-       * to fill the rest of the arena ;)
-      */
-      else if ((int)newSize < 1 && mem->fwd == NULL)
-      {
-        mem->size = (mem->size + sizeof(node_t) + newSize);
-      }
-      printf("...splitting not required\n");
-      printf("...updating chunk header at %p\n", mem);
-      printf("...being careful with my pointer arithmetic and void pointer casting\n");
-      printf("...allocation starts at %p\n", mem + 1);
-      return mem + 1;
+      if (mem->is_free == 1 && (mem->size >= size)) // first node free and big enough
+	{
+	  printf("...found free chunk of %d bytes with header at %p\n", (int)mem->size, mem);
+	  mem->is_free = 0;
+	  mem->size = size;
+	  printf("...free chunk->fwd currently points to %p\n", mem->fwd);
+	  printf("...free chunk->bwd currently points to %p\n", mem->bwd);
+	  printf("...checking if splitting is required\n");
+	  /* 
+	   * Below line: getting new size to pass into the fwd node stores
+	   * how many bytes of memory can be stored after current mem
+	   */
+	  size_t newSize = (_arena_size - ((size_t)mem - (size_t)_arena_start)) - (size + (2 * sizeof(node_t)));
+	  if (mem->fwd == NULL && ((int)newSize >= 1)) // last node
+	    {
+	      node_t *fwd = (node_t *)((char *)mem + size + sizeof(node_t));
+	      fwd->size = newSize;
+	      fwd->is_free = 1;
+	      fwd->fwd = NULL;
+	      fwd->bwd = mem;
+	      mem->fwd = fwd;
+	    }
+	  /* 
+	   * else not enough space & last node, set curr size 
+	   * to fill the rest of the arena ;)
+	   */
+	  else if ((int)newSize < 1 && mem->fwd == NULL)
+	    {
+	      mem->size = (mem->size + sizeof(node_t) + newSize);
+	    }
+	  printf("...splitting not required\n");
+	  printf("...updating chunk header at %p\n", mem);
+	  printf("...being careful with my pointer arithmetic and void pointer casting\n");
+	  printf("...allocation starts at %p\n", mem + 1);
+	  return mem + 1;
+	}
+      else
+	mem = mem->fwd;
     }
-    else
-      mem = mem->fwd;
-  }
 
   printf("...no such free chunk exists\n...setting error code\n");
   statusno = ERR_OUT_OF_MEMORY;
@@ -159,8 +159,22 @@ void wfree(void *ptr)
   header->is_free = 1; // set header to free
 
   printf("...checking if coalescing is needed\n");
-  if (header->fwd != NULL || header->bwd != NULL) // implement coalsecing here
-  {
+  // implement coalsecing here
+  if (header->fwd != NULL && header->fwd->is_free == 1){
+    header->size = header->size + header->fwd->size + sizeof(struct __node_t);
+    header->fwd = header->fwd->fwd;
+    if (header->fwd != NULL){
+      header->fwd->fwd->bwd = header;
+    }
+  }
+  if (header->bwd != NULL && header->bwd->is_free == 1){
+    printf("bwds\n");
+    header->bwd->size = header->size + header->bwd->size + sizeof(struct __node_t);
+    header->bwd->fwd = header->fwd;
+    if (header->fwd != NULL){
+      header->fwd->bwd = header->bwd;
+    }
+    printf("bwde\n");
   }
   else
     printf("...coalescing not needed.\n");
