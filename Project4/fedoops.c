cdd290dc1d8ce8cdba2121dcb400ec301a47e66c
sheehy98 <37912697+sheehy98@users.noreply.github.com>
2021-03-17 02:39:37 -0400

    Update fedoops.c

--- Project4/fedoops.c
+++ Project4/fedoops.c
@@ -26,200 +26,226 @@ pthread_mutex_t stationMtx = PTHREAD_MUTEX_INITIALIZER; // mutex for gettin onna
 pthread_mutex_t stationDoneMtx = PTHREAD_MUTEX_INITIALIZER; // mutex for gettin onna station
 pthread_mutex_t doneMtx = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;         // conditional :)
-pthread_cond_t busyCond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t teamConds[4] = {PTHREAD_COND_INITIALIZER, PTHREAD_COND_INITIALIZER, PTHREAD_COND_INITIALIZER, PTHREAD_COND_INITIALIZER};
 
 void incrementPackages(int teamNum)
 {
-    packagesCompleted++;
+  packagesCompleted++;
 
-    switch (teamNum)
+  switch (teamNum)
     {
     case 0:
-        bluePackages++;
-        break;
+      bluePackages++;
+      break;
     case 1:
-        redPackages++;
-        break;
+      redPackages++;
+      break;
     case 2:
-        greenPackages++;
-        break;
+      greenPackages++;
+      break;
     case 3:
-        yellowPackages++;
-        break;
+      yellowPackages++;
+      break;
     }
 
-    return;
+  return;
 }
 
 void *slaveAway(void *arg)
 {
-    workerNode *worker = ((workerNode *)arg);
-    package *workerPackage = NULL; // just to prevent ptr->package->whatever
-    station *currStation = NULL;
-    int workerWaiting = 0;
+  workerNode *worker = ((workerNode *)arg);
+  package *workerPackage = NULL; // just to prevent ptr->package->whatever
+  station *currStation = NULL;
+  station *nextStation = NULL;
+  int workerWaiting = 0;
 
-    int workerId = worker->workerId;
-    int workerTeam = worker->team;
-    char *teamName = worker->teamName;
+  int workerId = worker->workerId;
+  int workerTeam = worker->team;
+  char *teamName = worker->teamName;
+  pthread_cond_t *busyCond = &teamConds[workerTeam];
+    
 
-    while (1)
+  while (1)
     {
-      while (1) {
+      while (1)
+	{
 		
-	if (pileHead == NULL)
-	  {
-	    return NULL;
-	  }
+	  if (pileHead == NULL)
+	    {
+	      return NULL;
+	    }
 	
-	pthread_mutex_lock(&grabMtx);
-	// grabbing a package should be protected
-	if (worker->isFree)
-	  {
-	    break;
-	  }
-	else
-	  {
-	    printf("BUSY:  Currently not Worker %s #%d's turn\n",
-		   teamName, workerId);
-	    pthread_cond_wait(&busyCond, &grabMtx);
-	    pthread_mutex_unlock(&grabMtx);
-	  }
-      }
+	  pthread_mutex_lock(&grabMtx);
+	  // grabbing a package should be protected
+	  if (worker->isFree)
+	    {
+	      break;
+	    }
+	  else
+	    {
+	      //printf("BUSY:  Currently not Worker %s #%d's turn\n",
+	      //teamName, workerId);
+	      pthread_cond_wait(busyCond, &grabMtx);
+	      pthread_mutex_unlock(&grabMtx);
+	    }
+	}
 	
-	pileHead->ready = 0;
-	worker->isFree = 0;
-	worker->package = pileHead;      // store package into worker to work on
-	workerPackage = worker->package; // store reference to package in workerPackage
-	printf("GRAB:  Worker %s #%d grabbed package %d\n",
-	       teamName, workerId, workerPackage->packageNum);
-	pileHead = pileHead->nextPackage; // pile goes to next package for another worker to grab
-        
-        pthread_mutex_unlock(&grabMtx);
-
-        printf("STRT:  Worker %s #%d is starting to work on package %d\n",
-               teamName, workerId, workerPackage->packageNum);
-        printf("INST:  Package #%d has a total of %d instructions\n",
-               workerPackage->packageNum, workerPackage->instructionCount);
-
+      worker->isFree = 0;
+      worker->package = pileHead;      // store package into worker to work on
+      workerPackage = worker->package; // store reference to package in workerPackage
+      printf("GRAB:  Worker %s #%d grabbed package %d\n",
+	     teamName, workerId, workerPackage->packageNum);
+      printf("INFO:  ");
+        printf("[");
         for (int i = 0; i < workerPackage->instructionCount; i++)
         {
-            currStation = stations + (workerPackage->custInstructions[i] - 1);
-            printf("MOVE:  Worker %s #%d is moving Package #%d to Station %s\n",
-                   teamName, workerId, workerPackage->packageNum, currStation->stationName);
+            if (i)
+                printf(", ");
+	    int temp;
+	    temp = workerPackage->custInstructions[i]-1;
+            printf("%s", temp == WEIGHT ? "Weight" : temp == BARCODE ? "Barcode"
+		   : temp == XRAY      ? "X-ray"
+		   : "Jostle");
+        }
+        printf("]\n");
+      
+      pileHead = pileHead->nextPackage; // pile goes to next package for another worker to grab
+        
+      pthread_mutex_unlock(&grabMtx);
 
-            /* below is the conveyor system */
-            int stationFreeFlag = 1;
-            while (1)
-            {
-                pthread_mutex_lock(&stationMtx);
-                stationFreeFlag = currStation->isFree;
-                if (stationFreeFlag)
-                {
+      printf("STRT:  Worker %s #%d is starting to work on package %d\n",
+	     teamName, workerId, workerPackage->packageNum);
+      printf("INST:  Package #%d has a total of %d instructions\n",
+	     workerPackage->packageNum, workerPackage->instructionCount);
+      for (int i = 0; i < workerPackage->instructionCount; i++)
+	{
+	  nextStation = stations + (workerPackage->custInstructions[i] - 1);
+	  printf("MOVE:  Worker %s #%d is moving Package #%d to Station %s\n",
+		 teamName, workerId, workerPackage->packageNum, nextStation->stationName);
+
+	  /* below is the conveyor system */
+	  int stationFreeFlag = 1;
+	  while (1)
+	    {
+	      pthread_mutex_lock(&stationMtx);
+	      stationFreeFlag = nextStation->isFree;
+	      if (stationFreeFlag && i > 0){
+		pthread_mutex_lock(&stationDoneMtx);
+		printf("FREE:  Station %s is now free\n", currStation->stationName);
+		currStation->isFree = 1;
+		pthread_cond_broadcast(&cond);
+		pthread_mutex_unlock(&stationDoneMtx);
+	      }
+	      if (stationFreeFlag)
+		{
 		  usleep(rand() % (10000 - 1000 + 1) + 1000); // Conveyor belt - takes random time
-		  currStation->isFree = 0;
+		  nextStation->isFree = 0;
+		  currStation = nextStation;
 		  pthread_mutex_unlock(&stationMtx);
 		  break;
-                }
-                else
-                {
-                    printf("WAIT:  Station %s is currently busy. Worker %s #%d waiting with Package #%d\n",
-                           currStation->stationName, teamName, workerId, workerPackage->packageNum);
-                    pthread_cond_wait(&cond, &stationMtx);
-                    pthread_mutex_unlock(&stationMtx);
-                }
-            }
+		}
+	      else
+		{
+		  printf("WAIT:  Station %s is currently busy. Worker %s #%d waiting with Package #%d\n",
+			 nextStation->stationName, teamName, workerId, workerPackage->packageNum);
+		  pthread_cond_wait(&cond, &stationMtx);
+		  pthread_mutex_unlock(&stationMtx);
+		}
+	    }
 
-            printf("WORK:  Worker %s #%d working on Package #%d at Station %s\n",
-                   teamName, workerId, workerPackage->packageNum, currStation->stationName);
-            if (workerPackage->fragile == 1, strcmp(currStation->stationName, "Jostle") == 0)
-            {
-                printf("VLNT:  Package #%d is fragile! Worker %s #%d is shaking the sh*t out of it\n",
-                       workerPackage->packageNum, teamName, workerId);
-            }
+	  printf("WORK:  Worker %s #%d working on Package #%d at Station %s\n",
+		 teamName, workerId, workerPackage->packageNum, currStation->stationName);
+	  if (workerPackage->fragile == 1, strcmp(currStation->stationName, "Jostle") == 0)
+	    {
+	      printf("VLNT:  Package #%d is fragile! Worker %s #%d is shaking the sh*t out of it\n",
+		     workerPackage->packageNum, teamName, workerId);
+	    }
 
-	    usleep(rand() % (10000 - 1000 + 1) + 1000); // Work on package - random time
+	  usleep(rand() % (10000 - 1000 + 1) + 1000); // Work on package - random time
+	  printf("DONE:  Worker %s #%d is finished working on Package #%d at Station %s\n",
+		       teamName, workerId, workerPackage->packageNum, currStation->stationName);
+	}
 	    
-	    pthread_mutex_lock(&stationDoneMtx);
-            printf("DONE:  Worker %s #%d is finished working on Package #%d at Station %s\n",
-                   teamName, workerId, workerPackage->packageNum, currStation->stationName);
-            printf("FREE:  Station %s is now free\n", currStation->stationName);
-            currStation->isFree = 1;
-            pthread_cond_broadcast(&cond);
-            pthread_mutex_unlock(&stationDoneMtx);
-        }
-        pthread_mutex_lock(&doneMtx);
-        worker->nextWorker->isFree = 1;
-        incrementPackages(workerTeam);
-        printf("CMLT:  Worker %s #%d is finished working on Package #%d \n",
-               teamName, workerId, workerPackage->packageNum);
-        pthread_cond_broadcast(&busyCond);
-        pthread_mutex_unlock(&doneMtx); // grabbing a package should be protected
+      pthread_mutex_lock(&stationDoneMtx);
+      printf("FREE:  Station %s is now free\n", currStation->stationName);
+      currStation->isFree = 1;
+      pthread_cond_broadcast(&cond);
+      pthread_mutex_unlock(&stationDoneMtx);
+	    
+      pthread_mutex_lock(&doneMtx);
+      worker->nextWorker->isFree = 1;
+      incrementPackages(workerTeam);
+      printf("CMLT:  Worker %s #%d is finished working on Package #%d \n",
+	     teamName, workerId, workerPackage->packageNum);
+      pthread_cond_broadcast(busyCond);
+      pthread_mutex_unlock(&doneMtx);
     }
 }
 
 int main()
 {
-    workerNode *workersHead[4] = {NULL, NULL, NULL, NULL};
+  workerNode *workersHead[4] = {NULL, NULL, NULL, NULL};
 
-    // int packageCount = 0;
-    int randSeed = getSeed();
-    printf("Random seed is: %d\n", randSeed);
-    // sleep(1);
-    printf("Seeding the randomizer...\n");
-    // sleep(1);
-    srand(randSeed); // seed the randomizer
+  // int packageCount = 0;
+  int randSeed = getSeed();
+  printf("Random seed is: %d\n", randSeed);
+  // sleep(1);
+  printf("Seeding the randomizer...\n");
+  // sleep(1);
+  srand(randSeed); // seed the randomizer
 
-    // number of packageCount, randomly generated
-    int packageCount = rand() % (UPPER - LOWER + 1) + LOWER;
-    printf("Total number of packages to be processed is: %d\n", packageCount);
-    // sleep(1);
+  // number of packageCount, randomly generated
+  int packageCount = rand() % (UPPER - LOWER + 1) + LOWER;
+  printf("Total number of packages to be processed is: %d\n", packageCount);
+  // sleep(1);
 
-    // initialize everything
-    createPackages(packageCount, &pileHead);
-    createStations(stations);
+  // initialize everything
+  createPackages(packageCount, &pileHead);
+  //printPackages(&pileHead);
+  createStations(stations);
 
-    for (int i = 0; i < NUM_TEAMS; i++)
+  for (int i = 0; i < NUM_TEAMS; i++)
     {
-        createWorkers(&workersHead[i], i);
+      createWorkers(&workersHead[i], i);
     }
 
-    pthread_t *workerThreads = (pthread_t *)malloc(NUM_WORKERS * NUM_TEAMS * sizeof(pthread_t));
+  pthread_t *workerThreads = (pthread_t *)malloc(NUM_WORKERS * NUM_TEAMS * sizeof(pthread_t));
 
-    // spawn threads (40 threads, 4 teams 10 workers each team)
-    printf("Ready... get set... slave you corporate slaves :^)\n");
-    // sleep(1);
+  // spawn threads (40 threads, 4 teams 10 workers each team)
+  printf("Ready... get set... slave you corporate slaves :^)\n");
+  // sleep(1);
 
-    // printWorkers(&workersHead[0]);
-    int teamIndex = 0;
-    for (int i = 0; i < NUM_TEAMS; i++)
+  // printWorkers(&workersHead[0]);
+  int teamIndex = 0;
+  for (int i = 0; i < NUM_TEAMS; i++)
     {
-        for (int j = 0; j < NUM_WORKERS; j++)
+      for (int j = 0; j < NUM_WORKERS; j++)
         {
-            pthread_create(&workerThreads[teamIndex], NULL, slaveAway, workersHead[i]);
-            workersHead[i] = workersHead[i]->nextWorker;
-            teamIndex++;
+	  pthread_create(&workerThreads[teamIndex], NULL, slaveAway, workersHead[i]);
+	  workersHead[i] = workersHead[i]->nextWorker;
+	  teamIndex++;
         }
     }
 
-    // join threads
-    int joinIndex = 0;
-    for (int i = 0; i < NUM_TEAMS; i++)
+  // join threads
+  int joinIndex = 0;
+  for (int i = 0; i < NUM_TEAMS; i++)
     {
-        for (int j = 0; j < NUM_WORKERS; j++)
+      for (int j = 0; j < NUM_WORKERS; j++)
         {
-            pthread_join(workerThreads[joinIndex], NULL);
-            joinIndex++;
+	  pthread_join(workerThreads[joinIndex], NULL);
+	  joinIndex++;
         }
     }
 
-    printf("----------------------------------------------------------------\n");
-    printf("Packages requested: %d\tPackages completed: %d \n", packageCount, packagesCompleted);
-    printf("Blue Processed: %d\tRed Processed: %d\tGreen Processed: %d\tYellow Processed: %d\n",
-           bluePackages, redPackages, greenPackages, yellowPackages);
+  printf("----------------------------------------------------------------\n");
+  printf("Packages requested: %d\tPackages completed: %d \n", packageCount, packagesCompleted);
+  printf("Blue Processed: %d\tRed Processed: %d\tGreen Processed: %d\tYellow Processed: %d\n",
+	 bluePackages, redPackages, greenPackages, yellowPackages);
 
-    printf("Well done peasants. Shift is over :)\n");
+  printf("Well done peasants. Shift is over :)\n");
 
-    free(workerThreads);
+  free(workerThreads);
 
-    return 0;
+  return 0;
 }
